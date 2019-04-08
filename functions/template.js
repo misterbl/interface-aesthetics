15108cc2243b2447474cfcf06490994237c1ef2f
Laurent Bouhnik <laurent.bouhnik@justgiving.com>
2019-04-08 19:08:06 +0100

    next course div in carousel

--- functions/template.js
+++ functions/template.js
@@ -29,8 +29,6 @@ const template = opts => {
       <!-- Bootstrap -->
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
       <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
-      <!-- Custom CSS -->
-      <link rel="stylesheet" href="/assets/custom.css" />
       </head>
     <body>
       <div id="root">${opts.body}</div>
